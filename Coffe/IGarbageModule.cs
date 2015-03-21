ddcb39c08bd14893a707a9b3f96966f9d7cba9f2
S-LifeLover <l.rus-lifelover@mail.ru>
2015-03-21 13:35:07 +0300

    Added IGarbageModule

---
+++ Coffe/IGarbageModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Coffe
+{
+    interface IGarbageModule
+    {
+        // Ненужный комментарий
+        void Insert(int volume);
+
+        // Ненужный комментарий
+        void Clean(int volume);
+    }
+}
