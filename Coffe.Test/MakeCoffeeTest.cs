5d7b6e27c7f6fc8a57589af0da39e85c6287a523
muradgas <murad_gas@inbox.ru>
2015-03-26 09:07:50 +0300

    add makecoffee

---
+++ Coffe.Test/MakeCoffeeTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Coffe.Test
+{
+    [TestClass]
+    public class MakeCoffeeTests
+    {
+        [TestMethod]
+        public void DeleteWaterTest()
+        {           
+            var waterModule = new Mock<IWaterModule>();
+            var mainModule = new MainModule(waterModule.Object);
+            mainModule.DeleteWater();
+            waterModule.Verify(wm => wm.Delete(10));
+        }
+        [TestMethod]
+        public void DeleteCoffeeTest()
+        {
+            var coffeeModule = new Mock<ICoffeeModule>();
+            var mainModule = new MainModule(coffeeModule.Object);
+            mainModule.DeleteCoffee();
+            coffeeModule.Verify(cm => cm.Delete(20));
+        }
+        [TestMethod]
+        public void InsertGarbageTest()
+        {
+            //asfasfasfasdf
+            var garbageModule = new Mock<IGarbageModule>();
+            var mainModule = new MainModule(garbageModule.Object);
+            mainModule.InsertGarbage();
+            garbageModule.Verify(gm => gm.Insert(10));
+        }
+    }
+}
